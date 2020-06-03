bd72bfe71b5bc706e1f6d158b4efcaaaf80f4d96
peng4740 <35738277+peng4740@users.noreply.github.com>
2020-06-03 19:00:51 +0800

    Update openssh.sh

--- openssh.sh
+++ openssh.sh
@@ -1,9 +1,4 @@
-#####脚本使用#####
-#使用默认"1234qwer"为密码
-#bash openssh.sh
-####
-#自定义密码(passwd改为你需要设置的密码)
-#bash openssh.sh passwd
+#默认设置root密码为1234qwer
 
 #sshd_config配置文件备份
 cp /etc/ssh/sshd_config > /etc/ssh/sshd_config.bak
@@ -17,9 +12,4 @@ sed -i "s/#PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
 service ssh restart && service sshd restart && systemctl restart ssh && systemctl restart sshd && /etc/init.d/ssh restart && /etc/init.d/sshd restart
 
 #设置root密码
-if [ ! $1 ];then
-	pwd="1234qewr"
-else
-  pwd=$1
-fi
-echo root:$pwd | chpasswd
+echo root:1234qwer | chpasswd
